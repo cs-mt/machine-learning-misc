27cee71d65a8ea5357ddea843544442ff795eb52
cs-mt <110451185+cs-mt@users.noreply.github.com>
2023-03-09 01:18:09 +0300

    update

--- XOR-Perceptron/example.py
+++ XOR-Perceptron/example.py
@@ -20,17 +20,20 @@ model = tf.keras.Sequential()
 
 # This creates an input layer with 2 neurons, and a hidden layer with 4 neurons. (Multi layer perceptron)
 # The hidden layer requires at least 4 neurons for the model to accurately predict all 4 of the outputs of the XOR function according to my observations.
+
 model.add(tf.keras.layers.Dense(4, input_dim=2, activation="relu"))
 
 # This creates an output layer with 1 neuron.
 model.add(tf.keras.layers.Dense(1, activation="sigmoid"))
 
+#tf.keras.utils.plot_model(model, to_file='multi-layer.png', show_shapes=True, show_layer_names=True)
+
 
 # This creates an input layer with 2 neurons, and a single output neuron. (Single layer perceptron)
 # XOR is impossible with this model, but OR can easily be predicted.
-"""
 model.add(tf.keras.layers.Dense(1, input_dim=2, activation="sigmoid"))
-"""
+
+#tf.keras.utils.plot_model(model, to_file='single-layer.png', show_shapes=True, show_layer_names=True)
 
 opt = tf.keras.optimizers.Adam(learning_rate=0.002)
 model.compile(loss="binary_crossentropy", optimizer=opt, metrics=['accuracy'])
